e8802dc36318bbc50ece68564a7939b26ff80119
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2024-11-29 07:47:48 +0530

    Update _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -2,20 +2,27 @@ import { Stack } from 'expo-router';
 import "../global.css";
 import { LogBox } from 'react-native';
 
-
 export default function Layout() {
-  LogBox.ignoreLogs(["Warning: Failed prop type"])
+  LogBox.ignoreLogs(["Warning: Failed prop type"]);
+
   return (
     <Stack
-    screenOptions={
-      {
-        headerShown:false
-      }
-    }
+      screenOptions={{
+        headerShown: false,
+      }}
     >
-      <Stack.Screen name='MovieScreen'options={{
-        presentation:'fullScreenModal'
-      }}/>
-      </Stack>
+      <Stack.Screen
+        name="MovieScreen"
+        options={{
+          presentation: "fullScreenModal",
+        }}
+      />
+      <Stack.Screen
+        name="PersonScreen"
+        options={{
+          presentation: "fullScreenModal",
+        }}
+      />
+    </Stack>
   );
 }
